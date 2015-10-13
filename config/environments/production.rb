c9962e5f3735ecd2ca228a0aa9bf4646dcb86c28
latter-jacob <latter.jacob@gmail.com>
2015-10-13 15:49:39 -0400

    change to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
-  config.serve_static_assets = true
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
   config.log_level = :debug
   config.i18n.fallbacks = true
